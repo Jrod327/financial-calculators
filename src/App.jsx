d8ba75a301bb94e32a6cc51171f112f7f19deb5c
Jared Henry <113052919+Jrod327@users.noreply.github.com>
2023-07-21 12:47:57 -0400

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+//MAKE A WEBAPP THAT TAKES A PURCHASE AND CONVINCES YOU NOT TO BUY IT BY SHOWING A CHART.JS CHART
+//OF WHAT THAT PURCHASE PRICE COULD DO IF IT WAS INVESTED IN THE STOCK MARKET OR HYSA. ALSO SHOW
+//WHAT YOU'LL PAY ON A LOAN OVER TIME IN INTEREST VS PRINCIPAL AND HIGHLIGHT HOW MUCH EXTRA IS PAID IN INTEREST
+
+export default function App() {
+	const [principal, setPrincipal] = useState("");
+	const [interest, setInterest] = useState("");
+	const [timesCompounded, setTimesCompounded] = useState("");
+	const [time, setTime] = useState("");
+	const [showResult, setShowResult] = useState(false);
+
+	const result =
+		principal *
+		Math.pow(1 + interest / 100 / timesCompounded, timesCompounded * time);
+
+	function handleResult(e) {
+		e.preventDefault();
+		setShowResult(true);
+	}
+
+	function handleReset() {
+		setPrincipal("");
+		setInterest("");
+		setTimesCompounded("");
+		setTime("");
+		setShowResult(false);
+	}
+
+	return (
+		<div>
+			<form className="pure-form pure-form-aligned">
+				<fieldset>
+					<div className="pure-control-group">
+						<label>Initial investment</label>
+						<input
+							type="text"
+							value={principal}
+							onChange={e => setPrincipal(Number(e.target.value))}
+						/>
+					</div>
+					<div className="pure-control-group">
+						<label>Expected interest rate</label>
+						<input
+							type="text"
+							value={interest}
+							onChange={e => setInterest(Number(e.target.value))}
+						/>
+					</div>
+					<div className="pure-control-group">
+						<label>How many times is interest compounded per year?</label>
+						<input
+							type="text"
+							value={timesCompounded}
+							onChange={e => setTimesCompounded(Number(e.target.value))}
+						/>
+					</div>
+					<div className="pure-control-group">
+						<label>Years to compound?</label>
+						<input
+							type="text"
+							value={time}
+							onChange={e => setTime(Number(e.target.value))}
+						/>
+					</div>
+					<div className="pure-controls">
+						<button
+							type="submit"
+							className="pure-button pure-button-primary"
+							onClick={handleResult}>
+							Submit
+						</button>
+						{showResult && (
+							<h1>
+								After {time} year(s), your initial investment of {principal} at
+								a return of {interest}% per year would be worth $
+								{Math.round(result)}
+							</h1>
+						)}
+					</div>
+				</fieldset>
+			</form>
+
+			<button className="pure-button pure-button-primary" onClick={handleReset}>
+				Reset
+			</button>
+		</div>
+	);
+}
